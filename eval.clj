425e0bac6a10231fa79fa15efbd980f1139ca0ef
Søren Sjørup <soren.sjorup@deondigital.com>
2019-10-18 21:48:46 +0200

    added numbers

--- eval.clj
+++ eval.clj
@@ -4,14 +4,19 @@
     (let [value (get context form :not-found)]
       (if (= :not-found value)
         (throw (ex-info (str "Unable to resolve symbol: " (name form) " in this context") {:context context :form form})))
-      (first value))))
+      (first value))
+    (if (number? form)
+      form
+      (throw (ex-info "case not supported" {:context context :form form}))
+      )))
 
 (defn test-eval
   ([form] (test-eval {} form))
   ([context form] (meval (into {} (map (fn [[k v]] [k [v]]) context)) form)))
 
 (comment
+  (test-eval '5)
   (test-eval 'x)
-  (test-eval '{x nil} 'x)
+  (test-eval '{x 45} 'x)
   (test-eval '(let [x 5] x))
   )
