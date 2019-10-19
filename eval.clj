ca48a141f44e230ec15959c3de9fdfd13a504ef0
Søren Sjørup <soren.sjorup@deondigital.com>
2019-10-19 11:53:05 +0200

    change runtime value representation

--- eval.clj
+++ eval.clj
@@ -6,17 +6,21 @@
         (throw (ex-info (str "Unable to resolve symbol: " (name form) " in this context") {:context context :form form})))
       (first value))
     (if (number? form)
-      form
-      (throw (ex-info "case not supported" {:context context :form form}))
-      )))
+      {:num form}
+      (throw (ex-info "case not supported" {:context context :form form})))))
+
+(defn lift-value [value]
+  (let [evalue (eval value)]
+    (if (number? evalue)
+      {:num evalue}
+      (throw (ex-info "value could not be lifted" {:value evalue})))))
 
 (defn test-eval
   ([form] (test-eval {} form))
-  ([context form] (meval (into {} (map (fn [[k v]] [k [v]]) context)) form)))
+  ([context form] (meval (into {} (map (fn [[k v]] [k (lift-value v)]) context)) form)))
 
 (comment
   (test-eval '5)
-  (test-eval 'x)
+  (test-eval 'not-found)
   (test-eval '{x 45} 'x)
-  (test-eval '(let [x 5] x))
   )
