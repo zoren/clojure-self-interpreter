e2de62c05db1998a1808a1c1d9e1ca4d0fa34cfd
Søren Sjørup <soren.sjorup@deondigital.com>
2019-10-26 13:05:59 +0200

    allow all constants in the spec

--- eval.clj
+++ eval.clj
@@ -1,3 +1,16 @@
+;; Strings, numbers, characters, true, false, nil and keywords evaluate to themselves.
+;; https://clojure.org/reference/evaluation
+;;An empty list () evaluates to an empty list.
+
+;; Strings, numbers, characters, true, false, nil and keywords evaluate to themselves.
+(defn constant? [value]
+  (some #(% value)
+        [string?
+         number?
+         char?
+         (partial contains? #{true false nil})
+         keyword?]))
+
 (defn meval
   [context form]
   (if (symbol? form)
@@ -5,14 +18,14 @@
       (if (= :not-found value)
         (throw (ex-info (str "Unable to resolve symbol: " (name form) " in this context") {:context context :form form})))
       (first value))
-    (if (number? form)
-      {:num form}
+    (if (constant? form)
+      {:constant form}
       (throw (ex-info "case not supported" {:context context :form form})))))
 
 (defn lift-value [value]
   (let [evalue (eval value)]
-    (if (number? evalue)
-      {:num evalue}
+    (if (constant? evalue)
+      {:constant evalue}
       (throw (ex-info "value could not be lifted" {:value evalue})))))
 
 (defn test-eval
@@ -21,6 +34,8 @@
 
 (comment
   (test-eval '5)
+  (test-eval '"")
+  (test-eval ':h)
   (test-eval 'not-found)
   (test-eval '{x 45} 'x)
   )
