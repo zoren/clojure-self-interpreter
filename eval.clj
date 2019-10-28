76b8e7749b07d0326ee278af560909f7dffa7c3e
Søren Sjørup <soren.sjorup@deondigital.com>
2019-10-28 15:11:18 +0100

    rewrite WIP on currying

--- eval.clj
+++ eval.clj
@@ -10,79 +10,104 @@
          keyword?]))
 
 (defn meval
-  [context form]
-  (letfn [(throw-context [message]
+  [form]
+  (letfn [(throw-context [context message]
             (throw (ex-info message {:context context :form form})))
-          (meval' [form] (meval context form))]
+          (throw-syntax [message]
+            (throw (ex-info message {:form form})))]
     (cond
       (constant? form)
-      form
+      (fn [_context] form)
 
       (symbol? form)
-      (let [value (get context form :not-found)]
-        (if (= :not-found value)
-          (throw-context (str "Unable to resolve symbol: " (name form) " in this context")))
-        (:constant value))
+      (fn [context]
+        (let [value (get context form :not-found)]
+          (if (= :not-found value)
+            (throw-context context (str "Unable to resolve symbol: " (name form) " in this context")))
+          (:constant value)))
 
       (vector? form)
-      (mapv meval' form)
+      (let [eelem (mapv meval form)]
+        (fn [context] (mapv #(% context) eelem)))
 
       (map? form)
-      (reduce (fn [acc kv]
-                (let [[k v] (mapv meval' kv)]
-                  (if (contains? acc k) (throw-context (str "Duplicate key: " k)))
-                  (assoc acc k v))) {} form)
+      (let [eelem (mapv (fn [[k v]] (mapv meval [k v])) form)]
+        (fn [context]
+          (reduce
+           (fn [acc kv]
+             (let [[k v] (mapv #(% context) kv)]
+               (if (contains? acc k) (throw-context context (str "Duplicate key: " k)))
+               (assoc acc k v)))
+           {}
+           eelem)))
 
       (set? form)
-      (reduce (fn [acc e]
-                (let [elem (meval' e)]
-                  (if (contains? acc elem) (throw-context (str "Duplicate key: " elem)))
-                  (conj acc elem))) #{} form)
+      (let [eelem (mapv meval form)]
+        (fn [context]
+          (reduce
+           (fn [acc ce]
+             (let [elem (ce context)]
+               (if (contains? acc elem) (throw-context context (str "Duplicate key: " elem)))
+               (conj acc elem)))
+           #{}
+           eelem)))
 
       (list? form)
       (if (empty? form)
-        '()
+        (fn [_context] '())
         (case (name (first form))
           "if"
           (cond
-            (= (count form) 3) (if (meval' (nth form 1))
-                                 (meval' (nth form 2)))
-            (= (count form) 4) (if (meval' (nth form 1))
-                                 (meval' (nth form 2))
-                                 (meval' (nth form 3)))
-            (< (count form) 3) (throw-context "Too few arguments to if")
-            :else (throw-context "Too many arguments to if"))
+            (= (count form) 3)
+            (let [[ec et] (map meval (rest form))]
+              #(if (ec %) (et %)))
+            (= (count form) 4)
+            (let [[ec et ef] (map meval (rest form))]
+              #(if (ec %) (et %) (ef %)))
+            (< (count form) 3) (throw-syntax "Too few arguments to if")
+            :else (throw-syntax "Too many arguments to if"))
+
           "do"
-          (last (map meval' (rest form)))
+          (let [ces (map meval (rest form))]
+            (fn [context] (last (map #(% context) ces))))
+
           "let*"
           (do
             (if-not (vector? (nth form 1 nil))
-              (throw-context "Bad binding form, expected vector"))
+              (throw-syntax "Bad binding form, expected vector"))
             (if (odd? (count (nth form 1)))
-              (throw-context "Bad binding form, expected matched symbol expression pairs"))
-            (let* [bindings (partition 2 (nth form 1))
-                   context'
-                   (reduce (fn [acc pair]
-                             (if-not (symbol? (first pair))
-                               (throw-context (str "Bad binding form, expected symbol, got: " (first pair))))
-                             (assoc acc (first pair) {:constant (meval acc (second pair))})) context bindings)]
-              (last (map (partial meval context') (drop 2 form)))))
-          "fn*"
-          (let
-              [arity-folder
-               (fn [acc body]
-                 (if-not (or (empty? body) (list? body)) (throw-context nil))
-                 (let [params (first body)]
-                   (if-not (vector? params) (throw-context (str (.getName (type (nth form 1 nil))) " cannot be cast to clojure.lang.ISeq")))
-                   (if-not (every? symbol? params) (throw-context "fn params must be Symbols"))
-                   (if (acc (count params)) (throw-context "Can't have 2 overloads with same arity"))
-                   (assoc acc (count params) body)))]
-            {:fn (try (arity-folder {} (rest form))
-                      (catch Throwable _t
-                        (reduce arity-folder {} (rest form))))})
-          (throw-context "case not supported")))
+              (throw-syntax "Bad binding form, expected matched symbol expression pairs"))
+            (let [cbindings
+                  (mapv (fn [[bname bexp]]
+                          (if-not (symbol? bname)
+                            (throw-syntax (str "Bad binding form, expected symbol, got: " bname)))
+                          [bname (meval bexp)]
+                          ) (partition 2 (nth form 1)))
+                  body (last (drop 2 form))
+                  cbody (if (not= nil body) (meval body) (fn [_context] nil))]
+              (fn [context]
+                (cbody (reduce (fn [acc [k v]] (assoc acc k {:constant (v acc)}))
+                               context
+                               cbindings)))))
+
+          ;; "fn*"
+          ;; (let
+          ;;     [arity-folder
+          ;;      (fn [acc body]
+          ;;        (if-not (or (empty? body) (list? body)) (throw-context nil))
+          ;;        (let [params (first body)]
+          ;;          (if-not (vector? params) (throw-context (str (.getName (type (nth form 1 nil))) " cannot be cast to clojure.lang.ISeq")))
+          ;;          (if-not (every? symbol? params) (throw-context "fn params must be Symbols"))
+          ;;          (if (acc (count params)) (throw-context "Can't have 2 overloads with same arity"))
+          ;;          (assoc acc (count params) body)))]
+          ;;   {:fn (try (arity-folder {} (rest form))
+          ;;             (catch Throwable _t
+          ;;               (reduce arity-folder {} (rest form))))})
+
+          (throw-syntax "list case not supported")))
+
       :else
-      (throw-context "case not supported"))))
+      (throw-syntax "case not supported"))))
 
 (defn lift-value [value]
   (let [evalue (eval value)]
@@ -94,47 +119,49 @@
   (let [check
         #(try {:value (% form)} (catch Throwable e {:cause (:cause (Throwable->map e))}))
         reference (check eval)
-        this (check (partial meval {}))]
+        this (check (fn [form] ((meval form) {})))]
     (if (not= reference this)
       (throw (ex-info "Difference between reference and this implementation" {:form form :reference reference :this this})))))
 
 (def tests
   '["" 5 5.0 5.00M \a \" true false nil :kw
-    not-found ()
+    not-found
     [] [(if true 1)] [1 3 4]
     {} {1 (if true 1) (if true 1) 1} {:k 2 :v 5}
     #{} #{1 (if true 1)} #{1 2 3}
+    ()
     (if) (if nil) (if 0 1 2 3) (if false 1 2) (if nil 1 2) (if true 1 2) (if true 1) (if false 1)
-    (do) (do 4) (do 4 5)
-    (let*) (let* [x]) (let* [4 4] 5) (let* []) (let* [x 5] 6) (let* [x 5] 1 2 3)  (let* [x 5 y x] y)
+    (do) (do 4) (do 4 5) (do (if) 5)
+    (let*) (let* [x]) (let* [4 4] 5) (let* []) (let* [x 5] 6) (let* [x 5] x) (let* [x 5] 1 2 3)  (let* [x 5 y x] y)
     ])
 (doseq [f tests] (run-compare f))
 
-(defn run-compare-no-cause-compare [form]
-  (let [check
-        #(try {:value (% form)} (catch Throwable e {:cause (:cause (Throwable->map e))}))
-        reference (check eval)
-        this (check (partial meval {}))
-        throw-compare (fn [message] (throw (ex-info message {:form form :reference reference :this this})))]
-    (cond
-      (and (contains? reference :value) (contains? this :value))
-      (if (fn? (reference :value))
-        (if-not (contains? (:value this) :fn)
-          (throw-compare "Difference between reference and this implementation: function"))
-        (if (not= reference this)
-          (throw-compare "Difference between reference and this implementation")))
+;; (defn run-compare-no-cause-compare [form]
+;;   (let [check
+;;         #(try {:value (% form)} (catch Throwable e {:cause (:cause (Throwable->map e))}))
+;;         reference (check eval)
+;;         this (check (partial meval {}))
+;;         throw-compare (fn [message] (throw (ex-info message {:form form :reference reference :this this})))]
+;;     (cond
+;;       (and (contains? reference :value) (contains? this :value))
+;;       (if (fn? (reference :value))
+;;         (if-not (contains? (:value this) :fn)
+;;           (throw-compare "Difference between reference and this implementation: function"))
+;;         (if (not= reference this)
+;;           (throw-compare "Difference between reference and this implementation")))
 
-      (and (contains? reference :cause) (contains? this :cause))
-      nil
-      :else
-      (throw-compare "Difference between reference and this implementation"))))
+;;       (and (contains? reference :cause) (contains? this :cause))
+;;       nil
+;;       :else
+;;       (throw-compare "Difference between reference and this implementation"))))
 
-;; eval gives poor exception cause messages for fn* syntax errors so let's not copy those
-(def fn*-tests
-  '[(fn* {}) (fn* 3) (fn* "") (fn* [3]) (fn* ())
-    (fn*) (fn* [x]) (fn* [x] x) (fn* [x] 5 x)
-    (fn* ({})) (fn* ([]) ([])) (fn* ([x]) ([x]))
-    (fn* ([x] 1) ([x y] 2))
-    (fn* [] (if))
-    ])
-(doseq [f fn*-tests] (run-compare-no-cause-compare f))
+;; ;; eval gives poor exception cause messages for fn* syntax errors so let's not copy those
+;; (def fn*-tests
+;;   '[(fn* {}) (fn* 3) (fn* "") (fn* [3]) (fn* ())
+;;     (fn*) (fn* [x]) (fn* [x] x) (fn* [x] 5 x)
+;;     (fn* ({})) (fn* ([]) ([])) (fn* ([x]) ([x]))
+;;     (fn* ([x] 1) ([x y] 2))
+;;     ;; when bodies of functions have syntax errors they should be reported immediately not at call time
+;;     (fn* [] (if))
+;;     ])
+;; (doseq [f fn*-tests] (run-compare-no-cause-compare f))
