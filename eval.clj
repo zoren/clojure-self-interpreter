5dc0b424fad9b813b0b7cc52b8fb9332f1954a18
Søren Sjørup <soren.sjorup@deondigital.com>
2019-10-28 15:11:18 +0100

    rewrite meval to curry give syntax error messages early

--- eval.clj
+++ eval.clj
@@ -83,26 +83,34 @@
                             (throw-syntax (str "Bad binding form, expected symbol, got: " bname)))
                           [bname (meval bexp)]
                           ) (partition 2 (nth form 1)))
-                  body (last (drop 2 form))
-                  cbody (if (not= nil body) (meval body) (fn [_context] nil))]
+                  cbody (meval (last (drop 2 form)))]
               (fn [context]
                 (cbody (reduce (fn [acc [k v]] (assoc acc k {:constant (v acc)}))
                                context
                                cbindings)))))
 
-          ;; "fn*"
-          ;; (let
-          ;;     [arity-folder
-          ;;      (fn [acc body]
-          ;;        (if-not (or (empty? body) (list? body)) (throw-context nil))
-          ;;        (let [params (first body)]
-          ;;          (if-not (vector? params) (throw-context (str (.getName (type (nth form 1 nil))) " cannot be cast to clojure.lang.ISeq")))
-          ;;          (if-not (every? symbol? params) (throw-context "fn params must be Symbols"))
-          ;;          (if (acc (count params)) (throw-context "Can't have 2 overloads with same arity"))
-          ;;          (assoc acc (count params) body)))]
-          ;;   {:fn (try (arity-folder {} (rest form))
-          ;;             (catch Throwable _t
-          ;;               (reduce arity-folder {} (rest form))))})
+          "fn*"
+          (let
+              [arity-folder
+               (fn [acc body]
+                 ;; (if-not (or (empty? body) (list? body)) (throw-syntax ""))
+                 (let [params (first body)]
+                   (if-not (vector? params) (throw-syntax (str body params " cannot be cast to clojure.lang.ISeq")))
+                   (if-not (every? symbol? params) (throw-syntax "fn params must be Symbols"))
+                   (if (acc (count params)) (throw-syntax "Can't have 2 overloads with same arity"))
+                   (assoc acc (count params) (mapv meval (rest body)))))
+               m (try
+                   (arity-folder {} (rest form))
+                   (catch Throwable _t
+                     (reduce arity-folder {} (rest form))))]
+            (fn [context]
+              {:fn
+               ;;(try
+               m
+               ;; (catch Throwable _t
+               ;;   (reduce arity-folder {} (rest form)))
+               ;; )
+               }))
 
           (throw-syntax "list case not supported")))
 
@@ -121,7 +129,8 @@
         reference (check eval)
         this (check (fn [form] ((meval form) {})))]
     (if (not= reference this)
-      (throw (ex-info "Difference between reference and this implementation" {:form form :reference reference :this this})))))
+      (throw (ex-info "Difference between reference and this implementation"
+                      {:form form :reference reference :this this})))))
 
 (def tests
   '["" 5 5.0 5.00M \a \" true false nil :kw
@@ -130,38 +139,47 @@
     {} {1 (if true 1) (if true 1) 1} {:k 2 :v 5}
     #{} #{1 (if true 1)} #{1 2 3}
     ()
-    (if) (if nil) (if 0 1 2 3) (if false 1 2) (if nil 1 2) (if true 1 2) (if true 1) (if false 1)
+    (if) (if nil) (if 0 1 2 3) (if false (if)) (if false 1 2) (if nil 1 2) (if true 1 2) (if true 1) (if false 1)
     (do) (do 4) (do 4 5) (do (if) 5)
     (let*) (let* [x]) (let* [4 4] 5) (let* []) (let* [x 5] 6) (let* [x 5] x) (let* [x 5] 1 2 3)  (let* [x 5 y x] y)
     ])
 (doseq [f tests] (run-compare f))
 
-;; (defn run-compare-no-cause-compare [form]
-;;   (let [check
-;;         #(try {:value (% form)} (catch Throwable e {:cause (:cause (Throwable->map e))}))
-;;         reference (check eval)
-;;         this (check (partial meval {}))
-;;         throw-compare (fn [message] (throw (ex-info message {:form form :reference reference :this this})))]
-;;     (cond
-;;       (and (contains? reference :value) (contains? this :value))
-;;       (if (fn? (reference :value))
-;;         (if-not (contains? (:value this) :fn)
-;;           (throw-compare "Difference between reference and this implementation: function"))
-;;         (if (not= reference this)
-;;           (throw-compare "Difference between reference and this implementation")))
+(defn run-compare-no-cause-compare [form]
+  (let [check
+        #(try {:value (% form)} (catch Throwable e {:cause (:cause (Throwable->map e))}))
+        reference (check eval)
+        this (check (fn [form] ((meval form) {})))
+        throw-compare (fn [message] (throw (ex-info message {:form form :reference reference :this this})))]
+    (cond
+      (and (contains? reference :value) (contains? this :value))
+      (if (fn? (reference :value))
+        (if-not (contains? (:value this) :fn)
+          (throw-compare "Difference between reference and this implementation: function"))
+        (if (not= reference this)
+          (throw-compare "Difference between reference and this implementation")))
 
-;;       (and (contains? reference :cause) (contains? this :cause))
-;;       nil
-;;       :else
-;;       (throw-compare "Difference between reference and this implementation"))))
+      (and (contains? reference :cause) (contains? this :cause))
+      nil
+      :else
+      (throw-compare "Difference between reference and this implementation"))))
 
-;; ;; eval gives poor exception cause messages for fn* syntax errors so let's not copy those
-;; (def fn*-tests
-;;   '[(fn* {}) (fn* 3) (fn* "") (fn* [3]) (fn* ())
-;;     (fn*) (fn* [x]) (fn* [x] x) (fn* [x] 5 x)
-;;     (fn* ({})) (fn* ([]) ([])) (fn* ([x]) ([x]))
-;;     (fn* ([x] 1) ([x y] 2))
-;;     ;; when bodies of functions have syntax errors they should be reported immediately not at call time
-;;     (fn* [] (if))
-;;     ])
-;; (doseq [f fn*-tests] (run-compare-no-cause-compare f))
+;; eval gives poor exception cause messages for fn* syntax errors so let's not copy those
+(def fn*-tests
+  '[(fn* {}) (fn* 3) (fn* "") (fn* [3]) (fn* ())
+    (fn* [x]) (fn* [x] x) (fn* [x] 5 x)
+    (fn*) (fn* ({})) (fn* ([]) ([])) (fn* ([x]) ([x]))
+    (fn* ([x] 1) ([x y] 2))
+    ;; when bodies of functions have syntax errors they should be reported immediately not at call time
+    (fn* [] (if))
+    ])
+(doseq [f fn*-tests] (run-compare-no-cause-compare f))
+
+(comment
+  (def js-spec  '#{if def fn* do let* loop* letfn* throw try catch finally
+                   recur new set! ns deftype* defrecord* . js* & quote var})
+  (def java-spec '#{& monitor-exit case* try reify* finally loop* do letfn* if
+                    clojure.core/import* new deftype* let* fn* recur set! . var quote
+                    catch throw monitor-enter def})
+  (clojure.set/intersection js-spec java-spec)
+  )
